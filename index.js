43a464075c7d9527eba3e6c7ddebcd8eb722cbbc
unknown <duyka6203@gmail.com>
2024-01-12 04:35:11 +0700

    second commit

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
+var morgan = require('morgan')
 const app = express()
 const port = 3000
+
+app.use(morgan('combined'))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -9,6 +12,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
 })
-//npm Nodemon lắng nghe sự thay đổi trong các file để tự reload mà không cần thoát server rồi chạy lại: npm i nodemon --save-dev để cài trực tiếp vào depedencies của project mà không ảnh hưởng tới project khác và chỉ cần khi dev chứ không cần khi chạy thực tế
-//Node Package Manager: npm 
-//npm i express 
