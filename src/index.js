59f6c0a6205c6c8ebbe9d7b4cad09776d6e16c55
unknown <duyka6203@gmail.com>
2024-01-15 07:12:42 +0700

    add Bootstrap4

---
+++ src/index.js
@@ -0,0 +1,31 @@
+
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const port = 3000;
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')))
+// console.log(path.join(__dirname,'public'))
+//http logger
+app.use(morgan('combined'));
+
+//template engine
+app.engine('hbs', handlebars.engine({ 
+  // defaultLayout: 'main' ,
+  extname:'hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resourses\\views'))
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+app.get('/news', (req, res) => {
+  res.render('news')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
