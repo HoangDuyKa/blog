24af4e03a88c21e3c714b508b013e9d2be86cd22
unknown <duyka6203@gmail.com>
2024-01-16 21:34:28 +0700

    refactor code: add routes, app/controller folder

---
+++ src/app/controllers/SiteController.js
@@ -0,0 +1,12 @@
+class SiteController{
+    //[GET] /news
+    index(req, res){
+        res.render('home');
+    }
+    //[GET] /news/search
+    search(req,res){
+        res.render('search');
+    }
+}
+
+module.exports = new SiteController;
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 const handlebars = require('express-handlebars');
 const port = 3000;
 const app = express();
+const route = require('./routes');
 
 app.use(express.static(path.join(__dirname, 'public')))
 // console.log(path.join(__dirname,'public'))
@@ -19,16 +20,7 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resourses\\views'))
 
-app.get('/', (req, res) => {
-  res.render('home')
-});
-app.get('/news', (req, res) => {
-  res.render('news')
-});
-
-app.get('/search', (req, res) => {
-  res.render('search')
-});
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
