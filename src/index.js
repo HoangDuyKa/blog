6aa19918761b7008d2fb72e740ecaf92cf60dab0
unknown <duyka6203@gmail.com>
2024-01-18 04:04:18 +0700

    test husky 7

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ app.engine(
     }),
 );
 app.set('view engine', 'hbs');
-          app.set('views',         path.join(__dirname, 'resourses\\views'));
+  app.set('views', path.join(__dirname, 'resourses\\views'));
 
 route(app);
 
